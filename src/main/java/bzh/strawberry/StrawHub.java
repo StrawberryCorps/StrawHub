caca37b4e1b8d10e1f8b2f1c14a66012528a7f50
Uicias <clementbrd22@gmail.com>
2021-04-11 19:41:01 +0200

    init project

---
+++ src/main/java/bzh/strawberry/StrawHub.java
@@ -0,0 +1,11 @@
+package bzh.strawberry;
+
+/*
+This file StrawHub is part of a project StrawHub.
+It was created on 11/04/2021 at 19:40 by Uicias.
+This file as the whole project shouldn't be modify by others without the express permission from StrawHub author(s).
+Also this comment shouldn't get remove from the file. (see Licence.md)
+*/
+public class StrawHub {
+
+}
